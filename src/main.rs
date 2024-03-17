6f7ad31451caaebe06513d8d5f72951a5dfd034f
Russell Myers <mezner@russellmyers.com>
2024-03-17 11:45:33 -0400

    Add iced basic UI.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use iced::widget::{button, column, text, Column};
+
 #[derive(Default)]
 struct Counter {
     value: i32,
+}
+
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `Increment` message when pressed
+            button("+").on_press(Message::Increment),
+
+            // We show the value of the counter here
+            text(self.value).size(50),
+
+            // The decrement button. We tell it to produce a
+            // `Decrement` message when pressed
+            button("-").on_press(Message::Decrement),
+        ]
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
